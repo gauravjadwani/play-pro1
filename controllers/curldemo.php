5f315908c37185f9917053f469fff4f39eb72b8e
gauravjadwani <gaurav.jadwani@hotmail.com>
2016-11-15 14:47:33 +0530

    change on 2:47

--- controllers/curldemo.php
+++ controllers/curldemo.php
@@ -1,6 +1,7 @@
 <?php
 
-
+session_start();
+$_SESSION['name']='gaurav';
 $date=  array("name"=>"guarav");
 $u=http_build_query($date);
 
--- controllers/temp.php
+++ controllers/temp.php
@@ -1,9 +1,12 @@
 <?php
+//session_start();
+//$name=$_SESSION['name'];
 if(isset($_POST['postvar1']))
 {
 $e=$_POST['postvar1'];
 echo $e;
 echo 'true';
+//echo $name;
 
 }
 echo "g";
